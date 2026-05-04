eea76546a06a1f2fd8bbfe48889a58c146135e7a
sean <dltjdrnjs3@naver.com>
2026-05-04 15:24:11 +0900

    Update 456B.cpp

--- cpp/456/B/456B.cpp
+++ cpp/456/B/456B.cpp
@@ -2,43 +2,119 @@
 
 #define ll long long
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     vector<int> a(6);
     vector<int> b(6);
     vector<int> c(6);
-    for(int i=0; i<6; i++) {
-        cin>>a[i];
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> a[i];
     }
-    for(int i=0; i<6; i++) {
-        cin>>b[i];
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> b[i];
     }
-    for(int i=0; i<6; i++) {
-        cin>>c[i];
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> c[i];
     }
     int aCnt = 0;
-    for(int i=0; i<6; i++) {
-        if(a[i]==4 || a[i]==5|| a[i]==6) {
+    for (int i = 0; i < 6; i++)
+    {
+        if (a[i] == 4 || a[i] == 5 || a[i] == 6)
+        {
             aCnt++;
         }
     }
     int bCnt = 0;
-    for(int i=0; i<6; i++) {
-        if(b[i]==5||b[i]==6) {
+    for (int i = 0; i < 6; i++)
+    {
+        if (b[i] == 5 || b[i] == 6)
+        {
             bCnt++;
         }
     }
-    int cCnt =0;
-    for(int i=0; i<6; i++) {
-        if(c[i]==6) {
+    int cCnt = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        if (c[i] == 6)
+        {
             cCnt++;
         }
     }
+    if(aCnt==0) {
+        cout<<"0";
+        return 0;
+    }
     long double ans = 1;
-    ans*= (long double)aCnt / (long double)6;
-    ans*= (long double)bCnt / (long double) 6;
-    ans*= (long double)cCnt/(long double)6;
-    cout<<ans<<"\n";
+    ans *= (long double)aCnt / (long double)6;
+    ans *= (long double)bCnt / (long double)6;
+    ans *= (long double)cCnt / (long double)6;
+    long double ans2 = 1;
+    long double ans3 = 1;
+    aCnt = 0;
+    bCnt = 0;
+    cCnt = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        if (a[i] == 4 || a[i] == 5 || a[i] == 6)
+        {
+            cCnt++;
+        }
+    }
+    if(cCnt==0) {
+        cout<<"0";
+        return 0;
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        if (b[i] == 5 || b[i] == 6)
+        {
+            aCnt++;
+        }
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        if (c[i] == 6)
+        {
+            bCnt++;
+        }
+    }
+    ans2 *= (long double)aCnt / (long double)6;
+    ans2 *= (long double)bCnt / (long double)6;
+    ans2 *= (long double)cCnt / (long double)6;
+    aCnt = 0;
+    bCnt = 0;
+    cCnt = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        if (a[i] == 4 || a[i] == 5 || a[i] == 6)
+        {
+            bCnt++;
+        }
+    }
+    if(bCnt==0) {
+        cout<<"0";
+        return 0;
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        if (b[i] == 5 || b[i] == 6)
+        {
+            cCnt++;
+        }
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        if (c[i] == 6)
+        {
+            aCnt++;
+        }
+    }
+    
+    cout<<setprecision(12)<<ans<<"\n";
 }
