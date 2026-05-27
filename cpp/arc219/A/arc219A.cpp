c27a6dbef9140856bbea2a16fdf97c4ff39b92e8
sean <dltjdrnjs3@naver.com>
2026-05-27 18:34:10 +0900

    Update arc219A.cpp

--- cpp/arc219/A/arc219A.cpp
+++ cpp/arc219/A/arc219A.cpp
@@ -18,43 +18,35 @@ int main()
     {
         cin >> s[i];
     }
-    if (pow(2, m) == n)
+    if (pow(2ll, m) == (ll)n)
     {
         cout << "No\n";
         return 0;
     }
     sort(s.begin(), s.end());
-    string ans="";
-    for(int i=0; i<m; i++) {
-        ans+='0';
-    }
-    
-    for(int i=0; i<pow(2, m); i++) {
-        bool check = true;
-        for(int j=0; j<n; j++) {
-            bool flag = false;
-            for(int k=0; k<m; k++) {
-                if(ans[k]==s[j][k]) {
-                    flag = true;
-                    break;
+    cout<<"Yes\n";
+    string r ="";//결과
+    vector<bool> u(n, false);//해당 자리가 만족 했는가?
+    for(auto  i=0; i<m; i++) {
+        ll zero = 0;
+        ll one = 0;
+        for(auto  j=0; j<n; j++) {
+            if(!u[j]) {
+                if(s[j][i] =='0') {
+                    zero++;
+                }else {
+                    one++;
                 }
             }
-            if(!flag) {
-                check = false;
-                break;
-            }
         }
-        if(check) {
-            break;
-        }
-        for(int j=m-1; j>=0; j--) {
-            if(ans[j]=='0') {
-                ans[j]='1';
-                break;
-            } else {
-                ans[j]='0';
+        if(zero<one) r.push_back('1');
+        else r.push_back('0');
+        for(auto j=0; j<n; j++) {
+            if(s[j][i] == r[i]) {//입력 지점 == 결과랑 같으면-> 제외 (이건 이미 최소 한군대는 만족한거임, 다 제외시키고 안된곳만 보면 됨(false인 지점))
+                u[j]=true;
             }
         }
     }
-    cout<<"Yes\n"<<ans<<"\n";
+    cout<<r<<"\n";
+
 }
