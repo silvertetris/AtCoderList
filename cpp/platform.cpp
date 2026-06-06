9d1658a15ad4e0fe447229ed417f54adcc762ba0
sean <dltjdrnjs3@naver.com>
2026-06-06 22:52:14 +0900

    Create platform.cpp

---
+++ cpp/platform.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ld long double
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    
+}
